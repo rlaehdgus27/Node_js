6d432af3a44755e9d58346d8644adcccd62b8d03
rlaehdgus27 <rlaehdgus27@gmail.com>
2020-12-02 15:34:07 +0900

    updatae 3-5

---
+++ Chap3/3-5/ls.js
@@ -0,0 +1,11 @@
+const ls = require("child_process").exec;
+
+var process = ls("dir");
+
+process.stdout.on("data", function (data) {
+  console.log(data.toString());
+});
+
+process.stderr.on("data", function (data) {
+  console.error(data.toString());
+});
