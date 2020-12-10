2f836a525c1c13a7bb2c6d19e5b5143d07160937
rlaehdgus27 <rlaehdgus27@gmail.com>
2020-12-10 16:06:46 +0900

    Chap4, 4-1 update

---
+++ Chap4/4-1/server1.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/html; charset=uft-8" });
+    res.write("<h1> Hello Node! </h1>");
+    res.end("<p> Hello Server! </p>");
+  })
+  .listen(8080, () => {
+    // 서버연결
+    console.log("8080번 포트에서 서버 대기 중입니다");
+  });
