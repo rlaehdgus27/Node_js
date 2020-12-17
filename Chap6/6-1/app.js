5874bf8efd813c3941394a7526ed45d9444bfba8
rlaehdgus27 <rlaehdgus27@gmail.com>
2020-12-17 23:20:48 +0900

    Router update

--- Chap6/6-1/app.js
+++ Chap6/6-1/app.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config();
+const indexRouter = require("./routes");
+const userRouter = require("./routes/user");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
@@ -64,6 +66,14 @@ app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됩니다.");
   next();
 });
+
+app.use("/", indexRouter);
+app.use(" /user", userRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+
 app.get(
   "/",
   (req, res, next) => {
