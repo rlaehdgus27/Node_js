9fd9cec311ce54ed0ededcf6925f2a52443d2e99
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-13 15:21:59 +0900

    #3.4

--- WETUBE/index.js
+++ WETUBE/index.js
@@ -7,8 +7,16 @@ function handleListening() {
   console.log(`Listening on : http://localhost:${PORT}`);
 }
 
-app.get("/", function (req, res) {
-  res.send("hello world");
-});
+function handleHome(req, res) {
+  res.send("Hello");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
