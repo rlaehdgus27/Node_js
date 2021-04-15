b252600e3d9d0ee964dc4e3b5e0e11e8f7de47a9
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-15 23:49:17 +0900

    #3.21

--- WETUBE/routers/videoRouter.js
+++ WETUBE/routers/videoRouter.js
@@ -4,13 +4,11 @@ import {
   editVideo,
   upload,
   videoDetail,
-  videos,
 } from "../controllers/videoController";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
