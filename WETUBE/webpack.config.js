772cc276ecfe5148a9fc83cadeae5550e28a9249
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-25 22:18:16 +0900

    #5.3

--- WETUBE/webpack.config.js
+++ WETUBE/webpack.config.js
@@ -7,10 +7,14 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: ["babel-loader"],
+      },
       {
         test: /\.(scss)$/,
         use: [
@@ -42,6 +46,7 @@ const config = {
       },
     ],
   },
+  devtool: "source-map",
   output: {
     path: OUTPUT_DIR,
     filename: "[name].js",
