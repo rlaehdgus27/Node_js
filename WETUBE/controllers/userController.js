30a1b4613757cd29d49846dd86b2270f2ac7f2a3
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-26 20:42:30 +0900

    #7.6

--- WETUBE/controllers/userController.js
+++ WETUBE/controllers/userController.js
@@ -34,8 +34,12 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home,
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
 export const logout = (req, res) => {
-  // To Do: Process
+  req.logout();
   res.redirect(routes.home);
 };
 
