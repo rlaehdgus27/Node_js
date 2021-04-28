49ef24db785623c4b106cd9831b18104b7281682
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-28 22:25:11 +0900

    #8.1

--- WETUBE/routers/userRouter.js
+++ WETUBE/routers/userRouter.js
@@ -2,14 +2,17 @@ import express from "express";
 import routes from "../routes";
 import {
   changePassword,
-  editProfile,
+  getEditProfile,
+  postEditProfile,
   userDetail,
 } from "../controllers/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
+
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
