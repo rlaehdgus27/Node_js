dd09a285f666a10f2814a80e204e3e89ef2a1b6d
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-05-18 22:36:09 +0900

    #4.3 configureStore

--- REDUX/vanilla-redux/src/store.js
+++ REDUX/vanilla-redux/src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 
@@ -24,7 +24,7 @@ const reducer = createReducer([], {
     state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
