bf9dce1a48c5a5b6ca4bd3c8efd8aa401bf5cb74
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-08-15 19:57:01 +0900

    conf

--- BOARD/client/src/routes/Detail.jsx
+++ BOARD/client/src/routes/Detail.jsx
@@ -7,9 +7,7 @@ const Detail = ({}) => {
   const noticeDetail = async () => {
     const result = await axios.get(`http://127.0.0.1:4000/user/users/:id`);
 
-    console.log("sdfjhsf");
-    console.log("sdfjhsf");
-    console.log("sdfjhsf");
+    console.log(result);
 
     setDetail(result.data);
   };
